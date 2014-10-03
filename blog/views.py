da40f8cd567e8688d2fb01feab9e73609b84df99
Josh Wardini <joshwardini@gmail.com>
2014-10-02 21:06:01 -0700

    Added single post, added login

--- blog/views.py
+++ blog/views.py
@@ -7,6 +7,23 @@ from model import Post
 import mistune
 from flask import request, redirect, url_for
 
+from flask import flash
+from flask.ext.login import login_user
+from werkzeug.security import check_password_hash
+from model import User
+from flask.ext.login import login_required
+
+#setup logging
+import logging
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+console_handler.setFormatter(formatter)
+log.addHandler(console_handler)
+
+
 @app.route("/")
 @app.route("/page/<int:page>")
 def posts(page=1, paginate_by=10):
@@ -35,11 +52,13 @@ def posts(page=1, paginate_by=10):
     )
 
 @app.route("/post/add", methods=["GET"])
+@login_required
 def add_post_get():
     return render_template("add_post.html")
 
 
 @app.route("/post/add", methods=["POST"])
+@login_required
 def add_post_post():
     post = Post(
         title=request.form["title"],
@@ -48,4 +67,68 @@ def add_post_post():
     session.add(post)
     session.commit()
     return redirect(url_for("posts"))
+
+
+#Allows you to view a single post
+@app.route("/post/<int:post_id>")
+def view_post(post_id):
+    post = session.query(Post)
+    post = post.get(post_id + 1)
+    #log.info( "post = {}".format(post) )
+    return render_template("post.html",
+        post=post
+        )
+
+@app.route("/post/<int:post_id>/edit", methods = ["GET"])
+def edit_post(post_id):
+    post = session.query(Post).get(post_id + 1)
+    post_title = post.title
+    post_content = post.content
+    return render_template("edit_post.html",
+        post_title = post_title,
+        post_content = post_content
+        )
+
+@app.route("/post/<int:post_id>/edit", methods = ["POST"])
+def save_edit_post(post_id):
+    post = Post(
+        title=request.form["title"],
+        content=mistune.markdown(request.form["content"]),
+    )
+    session.add(post)
+    session.commit()
+    return redirect(url_for("posts"))
+
+#can't figure this out
+# @app.route("/post/<int:post_id>/delete", methods = ["POST"])
+# def delete_post(post_id):
+#     post = session.query(Post).get(post_id + 1)
+#     post_title = post.title
+#     return render_template("delete_post.html",
+#         post=post,
+#         post_title = post_title
+#         )
+
+@app.route("/login", methods=["GET"])
+def login_get():
+    return render_template("login.html")
+
+
+@app.route("/login", methods=["POST"])
+def login_post():
+    email = request.form["email"]
+    password = request.form["password"]
+    user = session.query(User).filter_by(email=email).first()
+    if not user or not check_password_hash(user.password, password):
+        flash("Incorrect username or password", "danger")
+        return redirect(url_for("login_get"))
+
+    login_user(user)
+    return redirect(request.args.get('next') or url_for("posts"))
+
+
+
+
+
+
 
--- manage.py
+++ manage.py
@@ -1,8 +1,18 @@
 import os
+
 from flask.ext.script import Manager
 
-
 from blog import app
+
+from getpass import getpass
+
+from werkzeug.security import generate_password_hash
+
+from blog.model import User
+
+from blog.model import Post
+
+from blog.database import session
 
 manager = Manager(app)
 
@@ -11,9 +21,6 @@ def run():
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
 
-
-from blog.model import Post
-from blog.database import session
 
 @manager.command
 def seed():
@@ -25,6 +32,24 @@ def seed():
             content=content
         )
         session.add(post)
+    session.commit()
+
+@manager.command
+def adduser():
+    name = raw_input("Name: ")
+    email = raw_input("Email: ")
+    if session.query(User).filter_by(email=email).first():
+        print "User with that email address already exists"
+        return
+
+    password = raw_input("Password ")
+    password_2 = raw_input("Password ")
+    while not (password and password_2) or password != password_2:
+        password = getpass("Password: ")
+        password_2 = getpass("Re-enter password: ")
+    user = User(name=name, email=email,
+                password=generate_password_hash(password))
+    session.add(user)
     session.commit()
 
 if __name__ == "__main__":
